0c50b75d6bf1ef44bd24f3a56615ee03ad0bd850
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-07-01 00:53:46 +0100

    Fix

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const date = new Date();
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+
+
 document.querySelector("#currentDay").textContent = daysOfWeek[date.getDay()]
-document.querySelector("#currentTime").textContent = date.toUTCString()
+
+
+const time = document.querySelector("#currentTime")
+
+time.textContent = date.toUTCString()
+
+
+setInterval(() => {
+      let date = new Date();
+      time.textContent = date.toUTCString()
+},10)
