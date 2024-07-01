b17be32c7f44f78c1449510453e647f61e2e3f7e
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-07-01 23:15:16 +0100

    Fix

--- script.js
+++ script.js
@@ -4,23 +4,19 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 
 
 const dateNode = document.querySelector("#currentDay")
-
-dateNode.textContent = daysOfWeek[date.getDay()]
+      
+      
+      
+      dateNode.textContent = daysOfWeek[date.getDay()]
 
 
 const time = document.querySelector("#currentTime")
-
-time.setAttribute("data-testid", date.toUTCString())
-dateNode.setAttribute("data-testid",daysOfWeek[date.getDay()])
 
 time.textContent = date.toUTCString()
 
 
 setInterval(() => {
       let date = new Date();
-      let daysofTheWeek = daysOfWeek[date.getDay()]
-      dateNode.setAttribute("data-testid",daysofTheWeek)
-      time.setAttribute("data-testid",date.toUTCString())
       time.textContent = date.toUTCString()
-      dateNode.textContent = daysofTheWeek
+      
 },10)
