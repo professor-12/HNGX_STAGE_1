90a415ada39471b6ec2698c8a7bf72ea162a52f9
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-07-01 13:03:06 +0100

    Create

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 
 
 const dateNode = document.querySelector("#currentDay")
-      
-      
-      
-      dateNode.textContent = daysOfWeek[date.getDay()]
+
+dateNode.textContent = daysOfWeek[date.getDay()]
 
 
 const time = document.querySelector("#currentTime")
+
+time.setAttribute("data-testid", date.toUTCString())
+dateNode.setAttribute("data-testid",daysOfWeek[date.getDay()])
 
 time.textContent = date.toUTCString()
 
 
 setInterval(() => {
-      dateNode.setAttribute("data-testid",daysOfWeek[date.getDay()]
-)
       let date = new Date();
-      time.textContent = date.toUTCString()
+      let daysofTheWeek = daysOfWeek[date.getDay()]
+      dateNode.setAttribute("data-testid",daysofTheWeek)
       time.setAttribute("data-testid",date.toUTCString())
+      time.textContent = date.toUTCString()
+      dateNode.textContent = daysofTheWeek
 },10)
